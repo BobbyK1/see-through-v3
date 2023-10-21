6a5b16170d9e51409df9159fc7cfec79c09e68dc
BobbyK1 <marbobkara@gmail.com>
2023-10-21 00:30:58 -0500

    Optimized barrel file loading

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    experimental: {
+        optimizePackageImports: ['@chakra-ui', '@chakra-ui/react', '@chakra-ui/next-js', '@emotion/react', '@emotion/styled', 'framer-motion']
+    }
 }
 
 module.exports = nextConfig
