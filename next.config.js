62df2bcade8061496e6a625b4bdbe45691d486c4
BobbyK1 <marbobkara@gmail.com>
2023-10-22 02:19:10 -0500

    Config fixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    experimental: {
+        optimizePackageImports: ['@chakra-ui', 'react-icons', '@chakra-ui/react', '@chakra-ui/next-js', '@emotion/react', '@emotion/styled', 'framer-motion', '@supabase/auth-helpers-nextjs', '@supabase/supabase-js']
+    }
 }
 
 module.exports = nextConfig
